e94326967943d9886687515266070de824adf01e
Goodness Okanlawon <102490495+CodeEvolver@users.noreply.github.com>
2022-12-12 14:10:05 +0100

    This is more better, commit it!

---
+++ index.js
@@ -0,0 +1,12 @@
+function validate() {
+    const x = document.getElementById('email-input').value;
+    const at = document.getElementById('email-input').value.indexOf("@");
+    if(x === "") {
+        document.getElementById('email-input').style.borderColor = "hsl(354, 100%, 66%)";
+        document.getElementById('error-message').style.color = "hsl(354, 100%, 66%)";
+        document.getElementById('error-message').innerHTML = "Whoops! It looks like you forgot to add your email";
+    }else if(at === -1) {
+        document.getElementById('error-message').style.color = "hsl(354, 100%, 66%)";
+        document.getElementById('error-message').innerHTML = "<i>Please provide a valid email address</i>";
+    }
+}
